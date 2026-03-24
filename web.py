d26d2d67718e5bff77d3574615a536f73ee94ef5
xuelin123yt <xuelin123yt@gmail.com>
2026-03-24 14:40:38 +0800

    flask_v1(新增math作業)

--- web.py
+++ web.py
@@ -8,8 +8,9 @@ def index():
     link += "<a href='/mis'>課程</a><hr>"
     link += "<a href='/today'>今天日期</a><hr>"
     link += "<a href='/about'>關於奕翔</a><hr>"
-    link += "<a href='/welcome?nick=奕翔&dep=靜宜資管'>傳送使用者暱稱</a><hr>"
-    link += "<a href=/account>奕翔簡介網頁</a><br>"
+    link += "<a href='/welcome?nick=奕翔&dep=靜宜資管'>GET傳值</a><hr>"
+    link += "<a href=/account>POST傳值(帳號密碼)</a><hr>"
+    link += "<a href=/math>數學運算</a><hr>" 
     return link
 
 @app.route("/mis")
@@ -48,5 +49,32 @@ def account():
     else:
         return render_template("account.html")
 
+@app.route("/math", methods=["GET", "POST"])
+def math():
+    if request.method == "POST":
+        x = int(request.form["x"])
+        opt = request.form["opt"]
+        y = int(request.form["y"])      
+        result = "您輸入的是：" + str(x) + opt + str(y)
+        
+        if (opt == "/" and y == 0):
+            result += "，除數不能為0"
+        else:
+            match opt:
+                case "+":
+                    r = x + y
+                case "-":
+                    r = x - y
+                case "*":
+                    r = x * y
+                case "/":
+                    r = x / y  # 修正：之前誤寫為 x - y
+                case _:
+                    return "未知運算符號"
+            result += "=" + str(r)  + "<br><a href=/>返回首頁</a>"          
+        return result
+    else:
+        return render_template("math.html")
+    
 if __name__ == "__main__":
     app.run()
