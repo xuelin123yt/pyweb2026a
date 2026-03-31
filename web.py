8f9837d97285fe3447411688ca9f6efa36954874
xuelin123yt <xuelin123yt@gmail.com>
2026-03-31 10:06:49 +0800

    擲茭

--- web.py
+++ web.py
@@ -1,3 +1,4 @@
+import random
 from flask import Flask, render_template, request
 from datetime import datetime
 app = Flask(__name__)
@@ -10,7 +11,8 @@ def index():
     link += "<a href='/about'>關於奕翔</a><hr>"
     link += "<a href='/welcome?nick=奕翔&dep=靜宜資管'>GET傳值</a><hr>"
     link += "<a href=/account>POST傳值(帳號密碼)</a><hr>"
-    link += "<a href=/math>數學運算</a><hr>" 
+    link += "<a href=/math>數學運算</a><hr>"
+    link += "<a href=/cup>擲茭</a><hr>"
     return link
 
 @app.route("/mis")
@@ -75,6 +77,34 @@ def math():
         return result
     else:
         return render_template("math.html")
+
+@app.route('/cup', methods=["GET"])
+def cup():
+    # 檢查網址是否有 ?action=toss
+    #action = request.args.get('action')
+    action = request.values.get("action")
+    result = None
     
+    if action == 'toss':
+        # 0 代表陽面，1 代表陰面
+        x1 = random.randint(0, 1)
+        x2 = random.randint(0, 1)
+        
+        # 判斷結果文字
+        if x1 != x2:
+            msg = "聖筊：表示神明允許、同意，或行事會順利。"
+        elif x1 == 0:
+            msg = "笑筊：表示神明一笑、不解，或者考慮中，行事狀況不明。"
+        else:
+            msg = "陰筊：表示神明否定、憤怒，或者不宜行事。"
+            
+        result = {
+            "cup1": "/static/" + str(x1) + ".jpg",
+            "cup2": "/static/" + str(x2) + ".jpg",
+            "message": msg
+        }
+        
+    return render_template('cup.html', result=result)
+
 if __name__ == "__main__":
     app.run()
