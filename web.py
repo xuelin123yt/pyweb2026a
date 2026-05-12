1a2ca66d3370efa465c41f1f8805f454d239dbc9
xuelin123yt <xuelin123yt@gmail.com>
2026-05-12 11:43:50 +0800

    week12_查詢電影分級機器人

--- web.py
+++ web.py
@@ -105,15 +105,30 @@ def rate():
 
 @app.route("/webhook3", methods=["POST"])
 def webhook3():
-    # build a request object
     req = request.get_json(force=True)
-    # fetch queryResult from json
-    action =  req.get("queryResult").get("action")
-    #msg =  req.get("queryResult").get("queryText")
-    #info = "動作：" + action + "； 查詢內容：" + msg
-    if (action == "rateChoice"):
-        rate =  req.get("queryResult").get("parameters").get("rate")
-        info = "我是王奕翔開發的電影聊天機器人,您選擇的電影分級是：" + rate + "，相關電影：\n"
+    action = req.get("queryResult").get("action")
+    
+    if action == "rateChoice":
+        rate = req.get("queryResult").get("parameters").get("rate")
+        info = "我是王奕翔開發的電影聊天機器人，您選擇的電影分級是：" + rate + "，相關電影：\n"
+        
+        db = firestore.client()
+        collection_ref = db.collection("本週新片含分級")
+        docs = collection_ref.get()
+        
+        result = ""
+        for doc in docs:
+            dict = doc.to_dict()
+            if rate in dict["rate"]:
+                result += "片名：" + dict["title"] + "\n"
+                result += "介紹：" + dict["hyperlink"] + "\n\n"
+        
+        if result == "":
+            result = "本週沒有符合該分級的電影"
+        
+        info += result
+    
+    return make_response(jsonify({"fulfillmentText": info}))
 
 @app.route("/weather", methods=["GET", "POST"])
 def weather():
