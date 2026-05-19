c82dcfd07cf4935f05b45d9096781b3d0b3a2678
xuelin123yt <xuelin123yt@gmail.com>
2026-05-19 10:47:32 +0800

    聊天機器人link

--- web.py
+++ web.py
@@ -39,7 +39,12 @@ def index():
     link += "<a href='/traffic'>查詢易肇事路口</a><hr>"
     link += "<a href='/weather'>天氣查詢</a><hr>"
     link += "<a href='/rate'>本週新片進DB</a><hr>"
+    link += "<a href='/demo'>聊天機器人</a><hr>"
     return link
+
+@app.route("/demo")
+def demo():
+    return render_template("demo.html")
 
 @app.route("/rate")
 def rate():
